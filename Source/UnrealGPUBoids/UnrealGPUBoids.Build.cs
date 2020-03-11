0b3ee50eb9824cca4a0793f86d46b91a04915c90
Timothy Davison <timtimmy@gmail.com>
2020-03-11 11:25:10 -0600

    Add modules for the RHI and RenderCore.

--- Source/UnrealGPUBoids/UnrealGPUBoids.Build.cs
+++ Source/UnrealGPUBoids/UnrealGPUBoids.Build.cs
@@ -8,7 +8,14 @@ public class UnrealGPUBoids : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[] {
+            "Core",
+            "CoreUObject",
+            "Engine",
+            "InputCore",
+            "RenderCore",
+            "RHI"
+        });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
